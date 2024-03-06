15a2f9c21da5df1954e7bb79a7affa23bb42926e
koway927 <77182820+koway927@users.noreply.github.com>
2024-03-06 02:00:59 -0600

    create a form using bootstrap

--- client/src/components/UserInfo.tsx
+++ client/src/components/UserInfo.tsx
@@ -1,13 +1,29 @@
 import "./UserInfo.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const UserInfo = () => {
   return (
-    <>
-      <p>
-        This is our first main component from where sub-components will be
-        developed.
-      </p>
-    </>
+    <div className="container">
+      <form>
+        <div className="form-group">
+          <label htmlFor="exampleInput">Input 1</label>
+          <input  className="form-control" id="exampleInput" placeholder="Enter information" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInput">Input 2</label>
+          <input  className="form-control" id="exampleInput" placeholder="Enter information" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInput">Input 3</label>
+          <input className="form-control" id="exampleInput" placeholder="Enter information" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInput">Input 4</label>
+          <input  className="form-control" id="exampleInput" placeholder="Enter information" />
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+  </div>
   );
 };
 
